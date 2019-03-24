34853bd53a19148cef0106857397fb51edae0568
Marco Marques <marco.marques@concrete.com.br>
2019-03-24 12:38:04 -0300

    Book Service

---
+++ BookStore/Controller/BookViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BookViewController.swift
+//  BookStore
+//
+//  Created by marco.h.maia.marques on 24/03/19.
+//  Copyright © 2019 marco.h.maia.marques. All rights reserved.
+//
+
+import UIKit
+
+final class BookViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BookStore/Services/APIService/BookService.swift
@@ -0,0 +1,38 @@
+//
+//  BookService.swift
+//  BookStore
+//
+//  Created by marco.h.maia.marques on 24/03/19.
+//  Copyright © 2019 marco.h.maia.marques. All rights reserved.
+//
+
+import Foundation.NSObject
+
+protocol BookServiceProtocol {
+    func fetchBook(name: String, page: Int, completion: @escaping (APIResult<SearchResponse>) -> Void)
+}
+
+final class BookService: NSObject, BookServiceProtocol {
+
+    private let apiClient: APIClientProtocol
+
+    override init() {
+        self.apiClient = APIClient()
+    }
+
+    init(apiClient: APIClientProtocol) {
+        self.apiClient = apiClient
+    }
+
+    func fetchBook(name: String, page: Int, completion: @escaping (APIResult<SearchResponse>) -> Void) {
+        let router = BookRouter.search(name: name, page: page)
+        apiClient.request(router: router){ (response: APIResult<SearchResponse>) in
+            switch response {
+            case let .success(value):
+                completion(.success(value))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
